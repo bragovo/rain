6b5d8d929843ea65c4b39c2c8ecab11997a3d8aa
Andrei Brizhinev <bragovo@gmail.com>
2025-04-30 19:18:57 +0300

    Baseline

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  plugins: [
+    'postcss-import',
+    'postcss-flexbugs-fixes',
+    [
+      "@csstools/postcss-global-data",
+      {
+        files: ['./app/vars.css']
+      }
+    ],
+    [
+      'postcss-preset-env',
+      {
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      features: {
+        'custom-media-queries': true,
+        'nesting-rules': true,
+        'color-mod-function': { unresolved: 'warn' },
+      },
+      stage: 3
+      }
+    ]
+  ]
+}
